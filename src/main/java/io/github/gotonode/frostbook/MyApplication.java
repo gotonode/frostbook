2f13d5cdc2873b75fc9cb61a1b21db1a5194196f
gotonode <5214152+gotonode@users.noreply.github.com>
2019-04-08 16:15:58 +0300

    Introductions

--- src/main/java/io/github/gotonode/frostbook/MyApplication.java
+++ src/main/java/io/github/gotonode/frostbook/MyApplication.java
@@ -3,6 +3,23 @@ package io.github.gotonode.frostbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Welcome to Frostbook!
+ *
+ * To view a live version of this project, please go to the following URL:
+ * <a href="https://frostbook.herokuapp.com/">Frostbook</a> (Heroku)
+ *
+ * Since it's hosted on a free Heroku dyno, please allow up to 10 seconds
+ * for the app to start on your first request. Subsequent requests will be
+ * served immediately (up until 30 minutes are up upon which the app sleeps).
+ *
+ * To view the source of this project in a more comfortable fashion, go to:
+ * <a href="https://github.com/gotonode/frostbook">Frostbook</a> (GitHub).
+ *
+ * Feedback is most welcome. Thank you in advance!
+ *
+ * - gotonode
+ */
 @SpringBootApplication
 public class MyApplication {
 
