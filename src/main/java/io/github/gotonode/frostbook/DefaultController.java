e4beeb5bcd4b39a914d96868ff76570bc0fbd0ae
gotonode <5214152+gotonode@users.noreply.github.com>
2019-04-08 16:55:24 +0300

    Basic style (CSS)

--- src/main/java/io/github/gotonode/frostbook/DefaultController.java
+++ src/main/java/io/github/gotonode/frostbook/DefaultController.java
@@ -7,9 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DefaultController {
 
-    @GetMapping("*")
+    @GetMapping("/")
     public String main(Model model) {
-
         return "index";
+    }
+
+    @GetMapping("/help")
+    public String help() {
+        return "help";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
     }
 }
