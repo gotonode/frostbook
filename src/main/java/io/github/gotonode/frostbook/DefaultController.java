12f68ec6107f56cfab1783ca64b2a9a4ff378044
gotonode <5214152+gotonode@users.noreply.github.com>
2019-04-08 17:51:28 +0300

    Heroku testing

--- src/main/java/io/github/gotonode/frostbook/DefaultController.java
+++ src/main/java/io/github/gotonode/frostbook/DefaultController.java
@@ -3,6 +3,9 @@ package io.github.gotonode.frostbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DefaultController {
@@ -20,5 +23,35 @@ public class DefaultController {
     @GetMapping("/about")
     public String about() {
         return "about";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/search")
+    public String searchPost(@RequestBody String query) {
+        System.out.println("Search query: " + query);
+
+        query = query.replace("query=", "");
+
+        query = query.trim();
+
+        return "redirect:/search?query=" + query;
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam String query, Model model) {
+
+        model.addAttribute("query", query.trim());
+
+        return "search";
+
     }
 }
