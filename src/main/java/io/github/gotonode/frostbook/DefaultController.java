39e48f66f3d53d39fcd6f8b94ce8073914cdf412
gotonode <5214152+gotonode@users.noreply.github.com>
2019-04-08 15:45:15 +0300

    Initial commit

---
+++ src/main/java/io/github/gotonode/frostbook/DefaultController.java
@@ -0,0 +1,15 @@
+package io.github.gotonode.frostbook;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("*")
+    public String main(Model model) {
+
+        return "index";
+    }
+}
---
+++ src/test/java/io/github/gotonode/frostbook/MainTest.java
@@ -0,0 +1,6 @@
+package io.github.gotonode.frostbook;
+
+import org.junit.Test;
+
+public class MainTest {
+}
