9c8613ee3ac3b4cac514b23421505fb661f0d3b6
gotonode <5214152+gotonode@users.noreply.github.com>
2019-04-28 13:14:22 +0300

    Create ProductionSecurityConfiguration.java

---
+++ src/main/java/io/github/gotonode/frostbook/security/ProductionSecurityConfiguration.java
@@ -0,0 +1,22 @@
+package io.github.gotonode.frostbook.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Profile("heroku")
+@Order(1)
+public class ProductionSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.requiresChannel()
+                .anyRequest().requiresSecure();
+
+        httpSecurity.headers().frameOptions().disable();
+    }
+}
